5f8f199c1a5235f7b699f9c85680a1a96dcf0c71
gabrielmoliva <moliva.gabriel@gmail.com>
2024-02-29 09:04:18 -0300

    Restante getters e setters Funcionario

--- models/Funcionario.java
+++ models/Funcionario.java
@@ -38,4 +38,60 @@ public class Funcionario{
     public void setDataNasc(Date dataNasc) {
         this.dataNasc = dataNasc;
     }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public status getString() {
+        return String;
+    }
+
+    public void setString(status string) {
+        String = string;
+    }
 }
