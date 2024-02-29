81660d4be3c182fce3aec9c82ca375af5bb25d5d
gabrielmoliva <moliva.gabriel@gmail.com>
2024-02-29 08:52:32 -0300

    atributos, getters e setters de funcionario

---
+++ models/Funcionario.java
@@ -0,0 +1,95 @@
+public class Funcionario{
+    protected String nome;
+    protected String cpf;
+    protected Date dataNasc;
+    protected String email;
+    protected String telefone;
+    protected char sexo;
+    protected String login;
+    protected String senha;
+    protected status String;
+
+    public Funcionario(nome, cpf, dataNasc, email, telefone, sexo, login, senha, status){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.dataNasc = dataNasc;
+        this.email = email;
+        this.telefone = telefone;
+        this.sexo = sexo;
+        this.login = login;
+        this.senha = senha;
+        this.status = status;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDataNasc() {
+        return dataNasc;
+    }
+
+    public void setDataNasc(Date dataNasc) {
+        this.dataNasc = dataNasc;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public status getString() {
+        return String;
+    }
+
+    public void setString(status string) {
+        String = string;
+    }
+}
