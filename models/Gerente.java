9030d492317883dcef08955649e9966a4d82b9ce
Rafael Takeguma <rafael08.0807@gmail.com>
2024-03-01 17:25:51 -0300

    importa Date em 'Gerente' e estende 'Funcionario' de 'OperadorCaixa'

--- models/Gerente.java
+++ models/Gerente.java
@@ -1,3 +1,5 @@
+import java.util.Date;
+
 public class Gerente extends Funcionario{
 
     public Gerente(String nome, String cpf, Date dataNasc, String email, String telefone, char sexo, String login, String senha, String status){
--- models/OperadorCaixa.java
+++ models/OperadorCaixa.java
@@ -1,4 +1,6 @@
-public class OperadorCaixa{
+import java.util.Date;
+
+public class OperadorCaixa extends Funcionario{
 
     public OperadorCaixa(String nome, String cpf, Date dataNasc, String email, String telefone, char sexo, String login, String senha, String status){
         super(nome, cpf, dataNasc, email, telefone, sexo, login, senha, status);
