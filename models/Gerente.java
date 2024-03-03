c854c4174b549f7187ee798503312f320251f756
LuanKY <83087980+LuanKY@users.noreply.github.com>
2024-03-03 14:53:31 -0300

    Delete models directory

--- models/Gerente.java
+++
@@ -1,8 +0,0 @@
-import java.util.Date;
-
-public class Gerente extends Funcionario{
-
-    public Gerente(String nome, String cpf, Date dataNasc, String email, String telefone, char sexo, String login, String senha, String status){
-        super(nome, cpf, dataNasc, email, telefone, sexo, login, senha, status);
-    }
-}
--- models/OperadorCaixa.java
+++
@@ -1,8 +0,0 @@
-import java.util.Date;
-
-public class OperadorCaixa extends Funcionario{
-
-    public OperadorCaixa(String nome, String cpf, Date dataNasc, String email, String telefone, char sexo, String login, String senha, String status){
-        super(nome, cpf, dataNasc, email, telefone, sexo, login, senha, status);
-    }
-}
